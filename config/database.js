8c2dc6c3c3ecd69425f8d7869549c114e120df1e
acodes617 <adocantocodes@gmail.com>
2022-05-30 22:29:18 -0400

    first message here!

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+  url: "mongodb+srv://adocanto:bostoncoder617@savagegangnem.g5xnb.mongodb.net/savage21auth?retryWrites=true&w=majority",
+  dbName: "savage21auth",
+};
